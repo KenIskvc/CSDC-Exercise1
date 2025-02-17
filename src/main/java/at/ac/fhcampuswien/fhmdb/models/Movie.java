12a6c4bf56a589cf86d3f0698dc43a4d4ebf6660
IhsanAtay <muhammed.atay@stud.fh-campuswien.ac.at>
2025-02-17 11:55:53 +0100

    Movie Filme eingefügt

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -33,6 +33,55 @@ public class Movie {
                 "Forrest Gump",
                 "The presidencies of Kennedy and Johnson, Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with an IQ of 75.",
                 List.of(Genre.DRAMA, Genre.ROMANCE)));
+        movies.add(new Movie(
+                "The Shawshank Redemption",
+                "Two imprisoned men bond over several years, finding solace and eventual redemption through acts of common decency.",
+                List.of(Genre.DRAMA)
+        ));
+
+        movies.add(new Movie(
+                "Inception",
+                "A thief who enters the dreams of others to steal secrets must plant an idea into a target's subconscious.",
+                List.of(Genre.ACTION, Genre.SCI_FI, Genre.THRILLER)
+        ));
+
+        movies.add(new Movie(
+                "The Godfather",
+                "The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
+                List.of(Genre.DRAMA, Genre.CRIME)
+        ));
+
+        movies.add(new Movie(
+                "Titanic",
+                "A young couple from different social classes fall in love aboard the ill-fated RMS Titanic.",
+                List.of(Genre.ROMANCE, Genre.DRAMA)
+        ));
+
+        movies.add(new Movie(
+                "The Dark Knight",
+                "When the Joker emerges from Gotham's underworld, Batman must navigate moral dilemmas to stop him.",
+                List.of(Genre.ACTION, Genre.CRIME, Genre.DRAMA)
+        ));
+        movies.add(new Movie(
+                "Pulp Fiction",
+                "The lives of two hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+                List.of(Genre.CRIME, Genre.DRAMA)
+        ));
+
+        movies.add(new Movie(
+                "Interstellar",
+                "A team of explorers travels through a wormhole in space in an attempt to ensure humanity's survival.",
+                List.of(Genre.SCI_FI, Genre.DRAMA, Genre.ADVENTURE)
+        ));
+
+        movies.add(new Movie(
+                "Schindler's List",
+                "In German-occupied Poland during World War II, industrialist Oskar Schindler saves the lives of more than a thousand Jewish refugees by employing them in his factories.",
+                List.of(Genre.DRAMA, Genre.HISTORY)
+        ));
+
+
+
 
 
 
