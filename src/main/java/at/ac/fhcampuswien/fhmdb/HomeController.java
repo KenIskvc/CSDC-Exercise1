e310ad233c42676f6131924b4a3ced08ea9e9106
harun sefer <harunseferoffice@gmail.com>
2025-02-20 18:00:48 +0100

    created filterByGenre() class and genres moved to GenreComboBox

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -6,6 +6,7 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
+import at.ac.fhcampuswien.fhmdb.models.Genre;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,10 +24,10 @@ public class HomeController implements Initializable {
     public TextField searchField;
 
     @FXML
-    public JFXListView movieListView;
+    public JFXListView<Movie> movieListView;
 
     @FXML
-    public JFXComboBox genreComboBox;
+    public JFXComboBox<String> genreComboBox;
 
     @FXML
     public JFXButton sortBtn;
@@ -43,7 +44,14 @@ public class HomeController implements Initializable {
         movieListView.setItems(observableMovies);   // set data of observable list to list view
         movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
 
-        // TODO add genre filter items with genreComboBox.getItems().addAll(...)
+        // Genres zur ComboBox hinzufügen
+        genreComboBox.getItems().add("Alle Genres"); // Option zum Zurücksetzen
+        for (Genre genre : Genre.values()) {
+            genreComboBox.getItems().add(genre.name());
+        }
+
+        // Event für Genre-Auswahl hinzufügen
+        genreComboBox.setOnAction(event -> filterByGenre());
         genreComboBox.setPromptText("Filter by Genre");
 
         // TODO add event handlers to buttons and call the regarding methods
@@ -61,5 +69,19 @@ public class HomeController implements Initializable {
         });
 
 
+    }
+    private void filterByGenre() {
+        String selectedGenre = (String) genreComboBox.getValue();
+
+        if (selectedGenre == null || selectedGenre.equals("Alle Genres")) {
+            observableMovies.setAll(allMovies); // Kein Filter -> Alle Filme anzeigen
+        } else {
+            Genre genreFilter = Genre.valueOf(selectedGenre);
+            observableMovies.setAll(
+                    allMovies.stream()
+                            .filter(movie -> movie.getGenres().contains(genreFilter))
+                            .toList()
+            );
+        }
     }
 }
